367d43d60df9509183390fc906724df80978bd6c
Efe <E.Sozen@student.tudelft.nl>
2021-05-05 16:00:54 +0200

    DataService class completed, http requests can be made and json is parsed correctly, creating an AkbankData object

--- lib/DataService.dart
+++ lib/DataService.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:finfree_akbank/AkbankData.dart';
 import 'dart:convert';
 import 'package:finfree_akbank/PriceEntry.dart';
 
@@ -9,12 +10,15 @@ class Service {
   final String authStr = 'R29vZCBMdWNr';
   final Uri uri = Uri.https(url, "/demo");
 
-  void getData() async {
+  Future<AkbankData> getData() async {
     Response resp = await get(uri, headers: {"authorization" : authStr });
     print(resp.statusCode);
     print(resp.body);
     if(resp.statusCode == 200){
-
+      dynamic body = json.decode(resp.body);
+      AkbankData data = AkbankData.fromJson(body);
+      //debugPrint(data.d_1h[0].c.toString());
+      return data;
     }else{
       throw "Couldn't fetch data";
     }
